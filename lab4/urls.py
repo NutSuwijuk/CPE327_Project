7aa153831dd15daef66f87cb57bb69bc1a2bbef9
NutSuwijuk <suwijuk2261@gmail.com>
2022-11-28 00:20:34 +0700

    normalLife

--- lab4/urls.py
+++ lab4/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('Location',views.Location),
     path('Number',views.Number),
     path('General',views.General),        
-    path('Person',views.Person),      
-    
+    path('Person',views.Person),  
+    path('NormalLife',views.NormalLife),
 ]
